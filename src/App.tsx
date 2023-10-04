9d7996d3985c006afd137a32a4d9f29bae745d6e
mukiwu <muki.wu@goodlinker.io>
2023-10-04 10:14:41 +0800

    feat: 增加自訂 html工具列

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
+import EditorToolbar, { modules, formats } from './components/EditorToolbar'
 import 'react-quill/dist/quill.snow.css'
 
 function App() {
   const [value, setValue] = useState('')
+  // const modules = {
+  //   toolbar: [
+  //     ['bold', 'italic', 'underline', 'strike'],
+  //     [{ header: 1 }, { header: 2 }],
+  //     [{ 'size': ['small', false, 'large', 'huge'] }]
+  //   ]
+  // }
 
   return (
-    <div className="w-full h-full">
-      <ReactQuill theme="snow" value={value} onChange={setValue} />
+    <div className="w-full">
+      <EditorToolbar />
+      <ReactQuill theme="snow" value={value} modules={modules} formats={formats} onChange={setValue} />
     </div>
   )
 }
