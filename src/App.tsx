34f778fac430808a1d5682a7e7780c4dcc9b518c
mukiwu <muki.wu@goodlinker.io>
2023-10-04 10:22:52 +0800

    增加文章連結

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
 import EditorToolbar, { modules, formats } from './components/EditorToolbar'
 import 'react-quill/dist/quill.snow.css'
+import { Tag } from 'antd'
 
 function App() {
   const [value, setValue] = useState('')
@@ -17,6 +18,9 @@ function App() {
     <div className="w-full">
       <EditorToolbar />
       <ReactQuill theme="snow" value={value} modules={modules} formats={formats} onChange={setValue} />
+      <div className="m-4 text-sm"><Tag color="magenta">Author</Tag>MUKI wu</div>
+      <div className="m-4 text-sm"><Tag color="magenta">Blog</Tag><a href="https://muki.tw/tech/react/quill-react-ant-design/" target="_blank" rel="noreferrer">https://muki.tw/tech/react/quill-react-ant-design/</a></div>
+      <div className="m-4 text-sm"><Tag color="magenta">Github</Tag><a href="https://github.com/mukiwu/quill-react-editor-with-ant-design" target="_blank" rel="noreferrer">https://github.com/mukiwu/quill-react-editor-with-ant-design</a></div>
     </div>
   )
 }
